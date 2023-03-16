e00d823bcd4ccf250974970e6701b100fa965144
Sielski, Bartosz (GlobalLogic) <bartosz.sielski@globallogic.com>
2023-03-16 20:49:14 +0100

    dockerfile

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = loads(request.data.decode('utf-8'))
+    if "name" not in store_data:
+        abort(400, message="Bad request")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -24,9 +27,13 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = loads(request.data.decode('utf-8'))
-    print(item_data)
-    if item_data["store_id"] not in stores:
-        abort(404, message="Store not found")
+    if ("price" not in item_data or
+            "store_id" not in item_data or
+            "name" not in item_data):
+        abort(400, message="Bad request")
+    for item in items.values():
+        if item_data["name"] == item['name'] and item_data['store_id'] == item['store_id']:
+            abort(400, message="Item already exist")
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
